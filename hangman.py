b21f0d35a7e93a4ee2999bbaa2cc9cee42bafb3e
slothTaco <pickeringwilliamscott@gmail.com>
2025-05-03 11:29:59 -0500

    Update hangman.py
    added hard, medium easy mode
    added multiple dictionary types: animals, shapes, colours and fruits

--- hangman.py
+++ hangman.py
@@ -1,57 +1,69 @@
 import random
+HANGMAN_PICS = ['''
+  +---+
+      |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+  |   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ /    |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O]  |
+ /|\  |
+ / \  |
+     ===''']
+words = {
+    'Colors':'red orange yellow green blue indigo violet white black brown'.split(),
+    'Shapes':'square triangle rectangle circle ellipse rhombus trapazoid chevron pentagon hexagon septagon octogon'.split(),
+    'Fruits':'apple orange lemon lime pear watermelon grape grapefruit cherry banana cantalope mango strawberry tomato'.split(),
+    'Animals':'bat bear beaver cat cougar crab deer dog donkey duck eagle fish frog goat leech lion lizard monkey moose mouse otter owl panda python rabbit rat shark sheep skunk squid tiger turkey turtle weasel whale wolf wombat zebra'.split()
+    }
 
-# hangman images
-hangman_pics = ['''
- +---+
-     |
-     |
-     |
-    ===''', '''
- +---+
- O   |
-     |
-     |
-    ===''', '''
- +---+
- O   |
- |   |
-     |
-    ===''', '''
- +---+
- O   |
-/|   |
-     |
-    ===''', '''
- +---+
- O   |
-/|\  |
-     |
-    ===''', '''
- +---+
- O   |
-/|\  |
-/    |
-    ===''', '''
- +---+
- O   |
-/|\  |
-/ \  |
-    ===''']
+def getRandomWord(wordDict):
+    # This function returns a random string from the passed dictionary of lists of strings, and the key also.
+    # First, randomly select a key from the dictionary:
+    wordKey = random.choice(list(wordDict.keys()))
 
-words = 'ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar',
-'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat', 'goose', 'hawk',
-'lion', 'lizard', 'llama', 'mole', 'monkey', 'moose', 'mouse', 'mule', 'newt', 'otter', 'owl', 'panda',
-'parrot', 'pigeon', 'python', 'rabbit', 'ram', 'rat', 'raven', 'rhino', 'salmon', 'seal', 'shark', 'sheep',
-'skunk', 'sloth', 'snake', 'spider', 'stork', 'swan', 'tiger', 'toad', 'trout', 'turkey', 'turtle',
-'weasel', 'whale', 'wolf', 'wombat', 'zebra'.split()
+    # Second, randomly select a word from the key's list in the dictionary:
+    wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
 
-def getRandomWord(wordList):
-    # This function returns a random string from the passed list of strings.
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+    return [wordDict[wordKey][wordIndex], wordKey]
 
 def displayBoard(missedLetters, correctLetters, secretWord):
-    print(hangman_pics[len(missedLetters)])
+    print(HANGMAN_PICS[len(missedLetters)])
     print()
 
     print('Missed letters:', end=' ')
@@ -70,7 +82,7 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     print()
 
 def getGuess(alreadyGuessed):
-    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
+    # Returns the letter the player entered. This function makes sure the player entered a single letter, and not something else.
     while True:
         print('Guess a letter.')
         guess = input()
@@ -85,27 +97,42 @@ def getGuess(alreadyGuessed):
             return guess
 
 def playAgain():
-    # This function returns True if the player wants to play again; otherwise, it returns False.
+    # This function returns True if the player wants to play again, otherwise it returns False.
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
 
 print('H A N G M A N')
+
+difficulty = ''
+while difficulty not in 'EMH':
+  print('Enter difficulty: E - Easy, M - Medium, H - Hard')
+  difficulty = input().upper()
+if difficulty == 'M':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+if difficulty == 'H':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandomWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 while True:
+    print('The secret word is in the set: ' + secretSet)
     displayBoard(missedLetters, correctLetters, secretWord)
 
-    # Let the player enter a letter.
+    # Let the player type in a letter.
     guess = getGuess(missedLetters + correctLetters)
 
     if guess in secretWord:
         correctLetters = correctLetters + guess
 
-        # Check if the player has won.
+        # Check if the player has won
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
@@ -118,7 +145,7 @@ while True:
         missedLetters = missedLetters + guess
 
         # Check if player has guessed too many times and lost.
-        if len(missedLetters) == len(hangman_pics) - 1:
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
             displayBoard(missedLetters, correctLetters, secretWord)
             print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
             gameIsDone = True
@@ -129,6 +156,7 @@ while True:
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
-            secretWord = getRandomWord(words)
+            secretWord, secretSet = getRandomWord(words)
         else:
             break
+
